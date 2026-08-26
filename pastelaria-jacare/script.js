da9ae72f5d5e46376ef02f25f7d4f0ab316f6a3c
mindseteigor-projects <mindsete.igor@gmail.com>
2026-08-26 15:22:20 -0300

    Add files via upload

---
+++ pastelaria-jacare/script.js
@@ -0,0 +1,35 @@
+const img = [
+    // imagens Base64 do arquivo original
+];
+
+const products = [
+    ['pastel','Pastel de Carne',
+    'Carne temperada, cebola e azeitona.',12.9],
+
+    ['pastel','Pastel de Queijo',
+    'Queijo muçarela derretido e orégano.',11.9],
+
+    ['pastel','Pastel de Frango com Catupiry',
+    'Frango desfiado, catupiry e temperos.',13.9],
+
+    ['pastel','Pastel de Pizza',
+    'Muçarela, presunto, tomate e orégano.',13.5],
+
+    ['pastel','Pastel de Calabresa',
+    'Calabresa, muçarela e cebola.',13.9],
+
+    ['pastel','Pastel de Carne com Queijo',
+    'Carne temperada e muçarela.',14.5],
+
+    ['pastel','Pastel de Palmito',
+    'Palmito cremoso, milho e ervas.',13.5],
+
+    ['pastel','Pastel de Bacon com Queijo',
+    'Bacon crocante e muçarela.',14.9],
+
+    ['pastel','Pastel de Chocolate',
+    'Chocolate cremoso e açúcar.',12.5],
+
+    ['pastel','Pastel Romeu e Julieta',
+    'Goiabada cremosa e queijo.',12.5]
+];
